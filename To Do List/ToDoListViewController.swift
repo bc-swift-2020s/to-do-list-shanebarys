c850c4e21ffcdd8d36bd11648752fd314e62be33
Shane Barys <shanejbarys@gmail.com>
2020-02-09 15:03:02 -0500

    5.1 added table view

---
+++ To Do List/ToDoListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Shane Barys on 2/9/20.
+//  Copyright © 2020 Shane Barys. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoArray = ["Learn Swift", "Change the World", "Take a Vacation", "Build Apps"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("numberOfRowsInSection was just called, returning \(toDoArray.count)")
+        return toDoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("cellForRowAt was just called for indexPath.row = \(indexPath.row) which is the cell containing \(toDoArray[indexPath.row])")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        return cell
+    }
+    
+    
+}
