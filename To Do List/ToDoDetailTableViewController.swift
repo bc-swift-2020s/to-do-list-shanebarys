5531b4d60bcfff4e0e0b4110e6648d0c97d038f3
Shane Barys <shanejbarys@gmail.com>
2020-02-09 16:02:09 -0500

    5.3 navigation controllers updated

---
+++ To Do List/ToDoDetailTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoDetailTableViewController.swift
+//  To Do List
+//
+//  Created by Shane Barys on 2/9/20.
+//  Copyright © 2020 Shane Barys. All rights reserved.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+
+    
+}
