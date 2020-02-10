6d5e2cc1bb1a7d54a150a14b8a1ec326add2eaa8
Shane Barys <shanejbarys@gmail.com>
2020-02-09 20:30:25 -0500

    5.6 structs

---
+++ To Do List/ToDoItem.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoItem.swift
+//  To Do List
+//
+//  Created by Shane Barys on 2/9/20.
+//  Copyright © 2020 Shane Barys. All rights reserved.
+//
+
+import Foundation
+
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
