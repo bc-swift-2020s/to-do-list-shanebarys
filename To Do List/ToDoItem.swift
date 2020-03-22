903c53317e840e63772b12525b0b43d7cd805dac
Shane Barys <shanejbarys@gmail.com>
2020-03-22 17:28:48 -0500

    5.10 local notifications

--- To Do List/ToDoItem.swift
+++ To Do List/ToDoItem.swift
@@ -14,4 +14,5 @@ struct ToDoItem: Codable {
     var date: Date
     var notes: String
     var reminderSet: Bool
+    var notificationID: String?
 }
--- To Do List/ToDoListViewController.swift
+++ To Do List/ToDoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ToDoListViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addBarButton: UIBarButtonItem!
     
@@ -21,75 +21,135 @@ class ToDoListViewController: UIViewController {
         tableView.delegate = self
         
         loadData()
+        authorizeLocalNotifications()
         // Do any additional setup after loading the view.
     }
-
-    func loadData() {
-        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
-        
-        guard let data = try? Data(contentsOf: documentURL) else {return}
-        let jsonDecoder = JSONDecoder()
-        do {
-            toDoItems = try jsonDecoder.decode(Array<ToDoItem>.self, from: data)
-        } catch {
-            print("Error, couldn't load data \(error.localizedDescription)")
-        }
-    }
     
-    func saveData() {
-        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
-        
-        let jsonEncoder = JSONEncoder()
-        let data = try? jsonEncoder.encode(toDoItems)
-        do {
-            try? data?.write(to: documentURL, options: .noFileProtection)
-        } catch {
-            print("Uh oh, couldn't save data \(error.localizedDescription)")
-        }
-        
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ShowDetail" {
-            let destination = segue.destination as! ToDoDetailTableViewController
-            let selectedIndexPath = tableView.indexPathForSelectedRow!
-            destination.toDoItem = toDoItems[selectedIndexPath.row]
-        } else {
-            if let selectedIndexPath = tableView.indexPathForSelectedRow {
-                tableView.deselectRow(at: selectedIndexPath, animated: true)
+    func authorizeLocalNotifications() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            guard error == nil else {
+                print("ERROR \(error?.localizedDescription)")
+                return
+            }
+            if granted {
+                print("Notification authorization granted")
+            } else {
+                print("User has denied notifications")
+                //TODO: put an alert in here telling the user what to do
             }
         }
     }
-
-
     
-    @IBAction func unwindFromDetail(segue: UIStoryboardSegue) {
-        let source = segue.source as! ToDoDetailTableViewController
-        if let selectedIndexPath = tableView.indexPathForSelectedRow {
-            toDoItems[selectedIndexPath.row] = source.toDoItem
-            tableView.reloadRows(at: [selectedIndexPath], with: .automatic)
-        } else {
-            let newIndexPath = IndexPath(row: toDoItems.count, section: 0)
-            toDoItems.append(source.toDoItem)
-            tableView.insertRows(at: [newIndexPath], with: .bottom)
-            tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
+    func setNotifications() {
+        guard toDoItems.count > 0 else {
+            return
         }
-        saveData()
-}
-
-    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
-        if tableView.isEditing {
-            tableView.setEditing(false, animated: true)
-            sender.title = "Edit"
-            addBarButton.isEnabled = true
-        } else {
-            tableView.setEditing(true, animated: true)
-            sender.title = "Done"
-            addBarButton.isEnabled = false
+        //remove all notifications
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        for index in 0..<toDoItems.count {
+            if toDoItems[index].reminderSet {
+                let toDoItem = toDoItems[index]
+                toDoItems[index].notificationID = setCalendarNotification(title: toDoItem.name, subtitle: "", body: toDoItem.notes, badgeNumber: nil, sound: .default, date: toDoItem.date)
+            }
         }
     }
+    
+    func setCalendarNotification(title: String, subtitle: String, body: String, badgeNumber: NSNumber?, sound: UNNotificationSound?, date: Date) -> String {
+        //create content
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.subtitle = subtitle
+        content.body = body
+        content.sound = sound
+        content.badge = badgeNumber
+        
+        //create trigger
+        var dateComponents = Calendar.current.dateComponents([.year, .day, .hour, .minute], from: date)
+        dateComponents.second = 00
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        //create request
+        let notificationID = UUID().uuidString
+        let request = UNNotificationRequest(identifier: notificationID, content: content, trigger: trigger)
+        
+        //register request with the notification center
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            } else {
+                print("Notification scheduled \(notificationID), title: \(content.title)")
+            }
+        }
+        return notificationID
+    }
+    
+
+func loadData() {
+    let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+    
+    guard let data = try? Data(contentsOf: documentURL) else {return}
+    let jsonDecoder = JSONDecoder()
+    do {
+        toDoItems = try jsonDecoder.decode(Array<ToDoItem>.self, from: data)
+    } catch {
+        print("Error, couldn't load data \(error.localizedDescription)")
+    }
+}
+
+func saveData() {
+    let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+    
+    let jsonEncoder = JSONEncoder()
+    let data = try? jsonEncoder.encode(toDoItems)
+    do {
+        try? data?.write(to: documentURL, options: .noFileProtection)
+    } catch {
+        print("Uh oh, couldn't save data \(error.localizedDescription)")
+    }
+    setNotifications()
+    }
+    
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "ShowDetail" {
+        let destination = segue.destination as! ToDoDetailTableViewController
+        let selectedIndexPath = tableView.indexPathForSelectedRow!
+        destination.toDoItem = toDoItems[selectedIndexPath.row]
+    } else {
+        if let selectedIndexPath = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: selectedIndexPath, animated: true)
+        }
+    }
+}
+
+
+
+@IBAction func unwindFromDetail(segue: UIStoryboardSegue) {
+    let source = segue.source as! ToDoDetailTableViewController
+    if let selectedIndexPath = tableView.indexPathForSelectedRow {
+        toDoItems[selectedIndexPath.row] = source.toDoItem
+        tableView.reloadRows(at: [selectedIndexPath], with: .automatic)
+    } else {
+        let newIndexPath = IndexPath(row: toDoItems.count, section: 0)
+        toDoItems.append(source.toDoItem)
+        tableView.insertRows(at: [newIndexPath], with: .bottom)
+        tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
+    }
+    saveData()
+}
+
+@IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+    if tableView.isEditing {
+        tableView.setEditing(false, animated: true)
+        sender.title = "Edit"
+        addBarButton.isEnabled = true
+    } else {
+        tableView.setEditing(true, animated: true)
+        sender.title = "Done"
+        addBarButton.isEnabled = false
+    }
+}
 }
 
 extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
@@ -121,5 +181,5 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     
-
+    
 }
